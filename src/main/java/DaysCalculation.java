ad4feb740c894ac72f8d2a6c669a80b601f6dcd3
Arunkumar C <arunkumar_c@external.mckinsey.com>
2020-01-02 15:44:09 +0530

    Adding logic to calculate number of weekdays and weekends

--- src/main/java/DaysCalculation.java
+++ src/main/java/DaysCalculation.java
@@ -6,14 +6,29 @@ public class DaysCalculation {
     public  DaysCalculation(){
         startCal = Calendar.getInstance();
         endCal = Calendar.getInstance();
+        endCal.add(Calendar.DATE,30);
     }
     public int numberOfWeekDaysInAMonth(){
-        return 0;
-
-
+        startCal = Calendar.getInstance();
+        int count=0;
+        while (startCal.getTimeInMillis()<=endCal.getTimeInMillis()){
+            if(startCal.get(Calendar.DAY_OF_WEEK)!=Calendar.SATURDAY&&startCal.get(Calendar.DAY_OF_WEEK)!=Calendar.SUNDAY){
+                count++;
+            }
+            startCal.add(Calendar.DATE,1);
+        }
+        return count;
     }
 
     public int numberOfWeekEndDaysInAMonth(){
-        return 0;
+        startCal = Calendar.getInstance();
+        int count=0;
+        while (startCal.getTimeInMillis()<=endCal.getTimeInMillis()){
+            if(startCal.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY||startCal.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY){
+                count++;
+            }
+            startCal.add(Calendar.DATE,1);
+        }
+        return count;
     }
 }
